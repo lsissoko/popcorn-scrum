15dc3d8ca32dafc00568a433530327e33f03c1bc
Lamine Sissoko <1082131+lsissoko@users.noreply.github.com>
2020-04-05 14:42:58 -0400

    read names from file

---
+++ README.md
@@ -0,0 +1,13 @@
+## popcorn-scrum
+
+First do this
+- navigate to wherever you cloned this repo (probably `cd <some-file-path-on-your-machine>/popcorn-scrum`)
+- create a `names.txt` file with your team members' names on separate lines
+
+Then do this
+- run `node popcorn.js`
+- read the prompt
+- enter the current speaker's number
+- repeat until you're done
+- type `exit` to exit, or ctrl+C, or ctrl+Z (whatever... I don't know your terminal)
+- profit!
--- popcorn.js
+++ popcorn.js
@@ -1,36 +1,45 @@
-let speakers = ['alice', 'bob', 'master roshi', 'castiel']
+const fs = require('fs')
+let speakers = []
 
 const logRemaining = () => {
   console.log('\nremaining speakers:')
+
+  // Iterating with Object.keys() because the array length never changes (the final array is [ <x empty items> ])
   Object.keys(speakers).forEach(idx => {
     console.log(`${idx} - ${speakers[idx]}`)
   })
 }
 
-const rl = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-const loop = () => {
+const loop = (reader) => {
   if (Object.keys(speakers).length <= 0) {
     console.log('everyone has spoken! goodbye')
-    return rl.close()
+    return reader.close()
   }
-  rl.question('\n\n\n> next speaker ("exit" to quit): ', (input) => {
+  reader.question('\n\n\n> next speaker ("exit" to quit): ', (input) => {
     input = input.replace(/\s/g, '').toLowerCase()
     if (input === 'exit') {
-      return rl.close()
+      return reader.close()
     } else if (Number.isInteger(parseInt(input))) {
       const idx = parseInt(input)
       console.log(`${speakers[idx].toUpperCase()} has spoken!`)
       delete speakers[idx]
       logRemaining()
     }
-    loop()
+    loop(reader)
   })
 }
 
-logRemaining()
-loop()
+fs.readFile('./names.txt', (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
 
+  speakers = data.toString().split('\n').filter(n => n.length)
+  logRemaining()
+
+  loop(require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  }))
+})
